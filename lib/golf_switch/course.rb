0822a39ac7867906883e99a15462fe17d56cd0b6
amardaxini <amardaxini@gmail.com>
2013-03-15 19:17:56 +0530

    fixed image url

--- lib/golf_switch/course.rb
+++ lib/golf_switch/course.rb
@@ -1,10 +1,11 @@
 module GolfSwitch
   class Course
     attr_accessor :id,:xid,:on_req,:nm,:cty,:st,:cou,:s_cou,:s_reg,:s_ar,:lat,:lon,:adv_days,:inside_days,:dist,:promo,:ratin,:img,:img_base
-
+    alias :name :nm
     def img_url
-      img_base.to_s+"/#{self.id}/"+img
+      #{img_base.to_s}/#{self.id}/#{img.to_s}"
     end
+
 
   end
 end
