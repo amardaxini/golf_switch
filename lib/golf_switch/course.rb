ee6133e16f6dd9d0f25bc79aef2a488bdbb9ca1e
amardaxini <amardaxini@gmail.com>
2013-03-15 19:33:41 +0530

    adede alias for course

--- lib/golf_switch/course.rb
+++ lib/golf_switch/course.rb
@@ -2,6 +2,14 @@ module GolfSwitch
   class Course
     attr_accessor :id,:xid,:on_req,:nm,:cty,:st,:cou,:s_cou,:s_reg,:s_ar,:lat,:lon,:adv_days,:inside_days,:dist,:promo,:ratin,:img,:img_base
     alias :name :nm
+    alias :country :cou
+    alias :state :st
+    alias :city :cty
+    alias :country_id :s_cou
+    alias :regon_id :s_reg
+    alias :area_id :s_ar
+
+
 
     def img_url
       "#{img_base.to_s}/#{self.id}/#{img.to_s}"
