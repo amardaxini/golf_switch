5c64b3eb7e48da9a2c16be76c7c28b94522fabda
amardaxini <amardaxini@gmail.com>
2013-03-18 15:21:33 +0530

    aded api for available golf course list

---
+++ examples/course_avail_list.rb
@@ -0,0 +1,16 @@
+require_relative "./../lib/golf_switch"
+GolfSwitch.configure do |config|
+  config.reseller_id="GHE"
+  config.partner_id=""
+  config.source_cd="A"
+  config.lang=""
+  config.user_ip="120.62.161.103"
+  config.user_session_id="some_session"
+  config.access_key="some_access_key"
+  config.gs_source=""
+end
+avail_list = GolfSwitch::CourseAvailList.new({:country_id=>"USA",:region_id=>"AZ",:play_beg_date=>Date.today.strftime("%Y-%m-%d"),:play_end_date=>Date.today.strftime("%Y-%m-%d")})
+avail_list.commit
+avail_list.parse_response
+binding.pry
+avail_list
